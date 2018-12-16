203a33f8892090822ceb248cfaa81da2f0e8c690
Wilko Wenzel <wilko.wenzel@outlook.com>
2018-12-16 18:15:03 +0100

    changed logging to winston

--- javascript/server/server.js
+++ javascript/server/server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+var {createLogger, format, transports} = require('winston');
+var logger = createLogger({
+   level:'debug',
+   format: format.combine(
+      format.colorize(),
+      format.timestamp({format:'YYYY-MM-DD HH:mm:ss'}),
+      format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)),
+   transports: [new transports.Console()]
+});
+
 app.use(express.json());
 
 var port = process.env.port || 3000;
@@ -16,7 +26,7 @@ var feedbacks = [
 
 app.get('/',(request, response) => {
    response.send(feedbacks);
-   console.log('done');
+   logger.info('received request for root of website');
 });
 
 app.post('/api/feedback/', (req, res) =>{
@@ -28,8 +38,10 @@ app.post('/api/feedback/', (req, res) =>{
       text:req.body.text
    };
 
+   logger.debug('received feedback');
+
    feedbacks.push(feedback);
    res.send(feedback);
 })
 
-app.listen(port,() => console.log(`Listening on port ${port}...`));
+app.listen(port,() => logger.info('Listening on port' + port));
